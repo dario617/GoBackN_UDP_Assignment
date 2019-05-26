85caf69cfdb83d685e0098df0fdb7eabae0717db
dario617 <spam.dario@gmail.com>
2019-05-25 20:46:50 -0400

    Added Go Back N with fixed timeout

--- client/client.py
+++ client/client.py
@@ -44,6 +44,15 @@ def main(ip, filename, window, packsize, seqsize, sendport, ackport):
 
         return "%s%s%s" % (str(seq_num_padded), str(checksum), message)
 
+    # Crear lista para manejar los Ack recibidos
+    # y listas para manejo de errores
+    acks = [False for el in parts]
+    ackCount = [0 for el in parts]
+    flags = [0]
+    lastAck = 0
+
+    # Timeout para la ventana calculado con el algoritmo de Karn
+    timeout = 0.9
 
     # Funcion anidada para leer el input.
     # Espera el paquete ack de parte del servidor. Esta versión no hace nada
@@ -63,8 +72,16 @@ def main(ip, filename, window, packsize, seqsize, sendport, ackport):
                 chksum = chksum.decode('utf-8')
                 if calculate_checksum(seq) == chksum:
                     print("Checksum correcto de seq: "+seq)
+                    acks[int(seq)] = True
+                    ackCount[int(seq)] = ackCount[int(seq)] + 1
                 else:
                     print("Checksum con errores")
+                # Si se recibe el elemento de la ventana muchas veces entonces 
+                # el checksum estaba corrupto pero el servidor obtuvo la info
+                # entonces podemos mover el seqnum
+                if ackCount[int(seq)] > 3 :
+                    print("Moviendo el numero de ACK a {}".format(int(seq)))
+                    flags[0] = int(seq)
 
     # Thread de recepción de acks.
     ack_thread = threading.Thread(target=receive_ack)
@@ -72,11 +89,38 @@ def main(ip, filename, window, packsize, seqsize, sendport, ackport):
 
     # Enviamos cada paquete. ¿Llegan siempre? No
     while seq_num < len(parts):
-        message = create_message(parts[seq_num], seq_num)
-        send_packet(ip, sendport, message)
-        time.sleep(0.1)
-        seq_num += 1
 
+        window_bottom = seq_num
+        if seq_num + window < len(parts):
+            window_top = seq_num + window
+        else:
+            window_top = len(parts)
+
+        # Enviar los paquetes en la ventana
+        print("Ventana actual",window_bottom, window_top)
+        while window_bottom < window_top:
+            message = create_message(parts[window_bottom], window_bottom)
+            send_packet(ip, sendport, message)
+            window_bottom += 1
+
+        # Esperar el timeout
+        time.sleep(timeout)
+
+        # Tomar ultimo valor True
+        while lastAck < len(parts) and acks[lastAck]:
+            lastAck += 1
+
+        # Compararlo con el valor de ack mas grande
+        if lastAck < flags[0]:
+            lastAck = flags[0]
+        
+        seq_num = lastAck
+
+    # Estadisticas finales de ejecucion
+    print("Acks recibidos sin problemas")
+    print(acks)
+    print("Conteo de acks")
+    print(ackCount)
     send_packet(ip, sendport, '') # Paquete vacio para terminar la conexion
 
 
@@ -85,7 +129,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", help="IP to send the data", default='127.0.0.1', type=str)
     parser.add_argument("--filename", help="File to send", default='mandela.txt', type=str)
-    parser.add_argument("--window", help="Window size", default=30, type=int)
+    parser.add_argument("--window", help="Window size", default=5, type=int)
     parser.add_argument("--packsize", help="Size of the package", default=30, type=int)
     parser.add_argument("--seqsize", help="Max sequence numbers", default=2, type=int)
     parser.add_argument("--sendport", help='Port to send the data', default=8989, type=int)
